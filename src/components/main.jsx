cf44e8821ef4350fd723b4f6b572225f2b9313f3
Kathy Hyde <musekat3@gmail.com>
2023-11-03 16:53:37 -0700

    files

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,3 +1,47 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import ErrorPage from './pages/error';
+import HomePage from './pages/homepage';
+import PortfolioPage from './pages/portfolio';
+import AboutPage from './pages/about';
+import ContactPage from '.pages/contact';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'portfolio/:id',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
+
---
+++ src/components/pages/about.jsx
@@ -0,0 +1,53 @@
+// This is a static page mocking an "About Us" section for our fake user data
+export default function AboutPage() {
+    return (
+      <div className="container pt-4">
+        <p>
+        
+        </p>
+        <section className="features-icons bg-light text-center m-4">
+          <div className="container">
+            <div className="row p-2">
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                  <div className="features-icons-icon d-flex">
+                    <i className="bi-window m-auto text-primary" />
+                  </div>
+                  <h3></h3>
+                  <p className="lead mb-0">
+                   
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                  <div className="features-icons-icon d-flex">
+                    <i className="bi-layers m-auto text-primary" />
+                  </div>
+                  <h3></h3>
+                  <p className="lead mb-0">
+                   
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-0 mb-lg-3">
+                  <div className="features-icons-icon d-flex">
+                    <i className="bi-terminal m-auto text-primary" />
+                  </div>
+                  <h3>Easy to Use</h3>
+                  <p className="lead mb-0">
+                   
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <p>
+          
+        </p>
+      </div>
+    );
+  }
+  
